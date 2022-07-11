e157dfba46eb25866884219dbd3dd091c1dbe0a0
Adelle Pitsas <apitsas4@gmail.com>
2022-07-11 16:51:32 -0400

    add understanding  of index positions

--- arrays.js
+++ arrays.js
@@ -1,3 +1,7 @@
 var teammates = ["Rachael", "Nick", "Pearce", "Lauren"]
 var points = [4, 6, 2, 3]
 var fromMaine = [false, true, false, false]
+
+//Index positions are in reference to the elements in an array, starting with 0.
+//For example, to call Nick:
+console.log(teammates[1])
